f7f6073a4a9766496dd553cde0be998f44426dd2
muiann <aimulyugina@yandex.ru>
2025-06-11 12:27:03 +0500

    init

---
+++ app/Components/TheFooter.tsx
@@ -0,0 +1,9 @@
+const TheFooter = () => {
+    return (
+        <footer className="container">
+Shop.co © 2000-2023, All Rights Reserved
+        </footer>
+    )
+}
+
+export {TheFooter};
---
+++ app/page.tsx
@@ -0,0 +1,2 @@
+export default function CatalogePage() {
+  return <h1>welcome shop</h1> }
